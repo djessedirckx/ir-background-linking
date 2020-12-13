7cdee59d1f56c3a7c1aba5489110b978a8bae327
Djesse Dirckx <djessedirckx@gmail.com>
2020-12-13 11:49:24 +0100

    Readded original reranker for testing

--- bglinking/reranker.py
+++ bglinking/reranker.py
@@ -9,16 +9,13 @@ import sys
 from pyserini import search
 from pyserini import index
 from pyserini import analysis
-import networkx as nx
-import matplotlib.pyplot as plt
 
 from bglinking.general_utils import utils
 from bglinking.database_utils import db_utils
 from bglinking.graph.graph import Graph
 from bglinking.graph.graph_comparators.GMCSComparator import GMCSComparator
 from bglinking.graph.graph_builders.DefaultGraphBuilder import DefaultGraphBuilder
-from bglinking.graph.graph_builders.ParagraphGraphBuilder import ParagraphGraphBuilder
-from bglinking.graph.graph_builders.NxBuilder import convert_to_nx, create_document_graph
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--index', dest='index', default='lucene-index.core18.pos+docvectors+rawdocs_all',
@@ -136,26 +133,14 @@ build_arguments = {'index_utils': index_utils,
 topics = utils.read_topics_and_ids_from_file(
     f'resources/topics-and-qrels/{args.topics}')
 
-paragraph_graph_builder = ParagraphGraphBuilder()
+default_graph_builder = DefaultGraphBuilder()
 
 for topic_num, topic in tqdm(topics):  # tqdm(topics.items()):
     query_num = str(topic_num)
     query_id = topic  # ['title']
 
-    bautista_doc_id = "1dd6be099ea95e49f4341b8e335acc30"
-    # query_graph = Graph(query_id, f'query_article_{query_num}', paragraph_graph_builder)
-    fname = f'query_article_{query_num}'
-    query_graph = Graph(bautista_doc_id, fname, paragraph_graph_builder)
-    result, par_ids = query_graph.build(**build_arguments)
-
-    # Convert all query paragraph graphs to nx graphs
-    graphs = list()
-    for i in range(len(result)):
-        paragraph_graph = convert_to_nx(par_ids[i], bautista_doc_id, result[i]) 
-        graphs.append(paragraph_graph)
-
-    # Create document graph
-    document_graph = create_document_graph(graphs, bautista_doc_id, fname)
+    query_graph = Graph(query_id, f'query_article_{query_num}', default_graph_builder)
+    query_graph.build(**build_arguments)
 
     # recalculate node weights using TextRank
     if args.textrank:
@@ -170,20 +155,12 @@ for topic_num, topic in tqdm(topics):  # tqdm(topics.items()):
         f'resources/candidates/{args.candidates}')
     for docid in qid_docids[query_num]:
         # Create graph object.
-        
         fname = f'candidate_article_{query_num}_{docid}'
-        candidate_graph = Graph(docid, fname, paragraph_graph_builder)
+        candidate_graph = Graph(docid, fname, default_graph_builder)
         candidate_graph.set_graph_comparator(comparator)
 
-        candidate_result, candidate_par_ids = candidate_graph.build(**build_arguments)
-        
-        # Convert all candidate paragraph graphs to nx graphs
-        candidate_graphs = list()
-        for i in range(len(candidate_result)):
-            paragraph_graph = convert_to_nx(candidate_par_ids[i], docid, candidate_result[i]) 
-            candidate_graphs.append(paragraph_graph)
-
-        candidate_document_graph = create_document_graph(graphs, docid, fname)
+        # Build (initialize) graph nodes and edges.
+        candidate_graph.build(**build_arguments)
 
         # recalculate node weights using TextRank
         if args.textrank:
@@ -192,7 +169,6 @@ for topic_num, topic in tqdm(topics):  # tqdm(topics.items()):
         relevance, diversity_type = candidate_graph.compare(
             query_graph, args.novelty, args.node_edge_l)
         ranking[docid] = relevance
-
         addition_types[docid] = diversity_type
 
     # Sort retrieved documents according to new similarity score.
@@ -227,7 +203,7 @@ for topic_num, topic in tqdm(topics):  # tqdm(topics.items()):
     utils.write_to_results_file(
         sorted_ranking, query_num, args.run_tag, f'resources/output/{args.output}')
 
-# if args.year != 20:
-#     # Evaluate performance with trec_eval.
-#     os.system(
-#         f"/opt/anserini-tools/eval/trec_eval.9.0.4/trec_eval -c -M1000 -m map -m ndcg_cut -m P.10 resources/topics-and-qrels/{args.qrels} resources/output/{args.output}")
+if args.year != 20:
+    # Evaluate performance with trec_eval.
+    os.system(
+        f"/opt/anserini-tools/eval/trec_eval.9.0.4/trec_eval -c -M1000 -m map -m ndcg_cut -m P.10 resources/topics-and-qrels/{args.qrels} resources/output/{args.output}")
